42b2d6aaa686ecc39025cca82145a48377f7794b
inmanCode <consolelog031@gmail.com>
2021-08-09 15:41:28 +0200

    header

---
+++ amazon-clone/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>amazon</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+    </div>
+  );
+}
