bfe65b838247d1d1ee277f2c7abf1e3c328d6a05
inmanCode <consolelog031@gmail.com>
2021-08-09 18:00:41 +0200

    main

--- amazon-clone/pages/index.js
+++ amazon-clone/pages/index.js
@@ -1,13 +1,29 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-export default function Home() {
+import Banner from '../components/Banner';
+import ProductFeed from '../components/ProductFeed';
+export default function Home({ products }) {
   return (
     <div>
-      <Head>
+      <Head className=' bg-gray-100'>
         <title>amazon</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
+      <main className='max-w-screen-2xl mx-auto'>
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+export async function getServerSideProps() {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products: products,
+    },
+  };
 }
